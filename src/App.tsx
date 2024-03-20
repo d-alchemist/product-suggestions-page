ce53eb2a0c60b882ab0c5650512d76f7e27a0ce0
Chilo <chilo.nwokolo@gmail.com>
2024-03-20 08:17:32 +0100

    update padding for all screen sizes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Suggestions from "./components/Suggestions";
 function App() {
   return (
     <>
-      <div className="grid grid-cols-7 gap-10 px-5 lg:px-0">
+      <div className="grid grid-cols-7 gap-10">
         <div className="lg:col-span-2 col-span-7">
           <SideNav />
         </div>
